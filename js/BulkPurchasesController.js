5b1b5e6e14a65cd95024cb5dcdd144d169e538e7
Alex Kaufman and Lizzie Koehler <lizziefkoehler@gmail.com>
2015-02-23 16:27:13 -0800

    add autoscroll

--- js/BulkPurchasesController.js
+++ js/BulkPurchasesController.js
@@ -1,11 +1,19 @@
 bulkShirts.controller('ShirtsCtrl', function ShirtsCtrl($scope) {
   $scope.shirts = [];
+  $scope.addShirt = function() {
+    $scope.shirts.push({ size: "{{size}}", color: "{{color}}"} )
+  };
 
-
-$(document).ready(function() {
-  $("img").click(function() {
-    $(this).toggleClass("click");
+  $(document).ready(function() {
+    $("img").click(function() {
+      $(this).toggleClass("click");
+      $('html, body').animate({
+      scrollTop: $("#color").offset().top
+      }, 1000);
+    });
   });
-});
+    $(".color").click(function() {
+      $(this).toggleClass("click");
+    });
 
 });
